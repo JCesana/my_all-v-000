a85f5ec909d1fcab77c2ddd80be21bd39dd7fba5
Josh Cesana <joshuacesana@gmail.com>
2018-05-19 23:17:24 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -9,7 +9,7 @@ def my_all?(collection)
     i += 1 
   end 
   
-  if block_return_values.includes?(false) || block_return_values = []
+  if block_return_values.include?(false)
     false
   else
     true 
